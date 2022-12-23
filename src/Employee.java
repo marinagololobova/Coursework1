247f0c8c4dd6d7391f22719c846bd8f78ecfa860
marinagololobova <marinagololobova@mail.ru>
2022-12-24 01:28:38 +0300

    Курсовая 1

---
+++ src/Employee.java
@@ -0,0 +1,49 @@
+
+public class Employee {
+
+    private String fullName;
+    private int department;
+    private double salary;
+    private static int counter = 1;
+    private int id;
+
+    public Employee(String fullName, int department, double salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО: " + fullName + ", отдел: " + department + ", зарплата: " + salary + ", id: " + id;
+    }
+}
